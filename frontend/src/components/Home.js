39a030b02a2b8275b93c3972e3c886fdbe88cc35
Tr1d3nt <saudagha2@gmail.com>
2022-03-23 19:27:58 -0600

    added city searching

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,10 +5,46 @@ const Home = () => {
     const data = [
         'Josh',
         'Brady'];
+
+    const [cities, setCities] = useState('');
+
+    const getCities = () => {
+
+        fetch('cities.json', {
+            headers: {
+
+
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+
+        }).then(function (response) {
+            console.log(response);
+            return response.json();
+        }).then(function (myJson) {
+
+            var data = [];
+            for (var i in myJson)
+                data.push([myJson[i].name]);
+
+            console.log(data);
+            setCities(data);
+        });
+
+
+
+
+    }
+
+    useEffect(() => {
+        getCities()
+    }, [])
+
     return (
         <div>
             <h2 style={{ textAlign: "center" }}>Book Flight</h2>
-            <AutoCompleteText items={data} />
+            <AutoCompleteText items={cities} />
+
         </div>
     );
 }
