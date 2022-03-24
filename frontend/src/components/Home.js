12dff2e2081480b85b354ac7346ed891799f051e
Tr1d3nt <saudagha2@gmail.com>
2022-03-23 18:34:26 -0600

    added autocompelte text

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react';
+import AutoCompleteText from './AutoCompleteText';
 
 const Home = () => {
+    const data = [
+        'Josh',
+        'Brady'];
     return (
         <div>
-            <h2>Home</h2>
+            <h2 style={{ textAlign: "center" }}>Book Flight</h2>
+            <AutoCompleteText items={data} />
         </div>
     );
 }
